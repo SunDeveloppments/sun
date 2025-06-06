102c205e0d3dcd5288b22bc911ad17b8f705bae6
Jellyfish <oscar_b@disroot.org>
2025-06-06 21:29:29 +0200

    🚧  Commit for add a init author and language feature

--- src/init.go
+++ src/init.go
@@ -6,20 +6,61 @@ import (
 	"io"
 )
 
+
+var key string
+
+func CreateFile(){
+	
+         f, err := os.Create("./.sunenv.yaml")
+   
+         if err != nil {
+   
+             fmt.Println(err)
+   
+         }
+   
+   defer f.Close()
+}
+
+func WriteYaml(key string){
+
+//	CreateFile()
+	 f, err := os.Create("./.sunenv.yaml")
+   
+            if err != nil {
+   
+                fmt.Println(err)
+    
+	         }
+
+       defer f.Close()
+
+	//arg := os.Args[1]
+    
+    var file_content string = fmt.Sprintf("name: %s", key)
+    
+    io.WriteString(f, file_content)
+}
+
 func Init(){
+
 	var arg string = os.Args[2]
 
-	f, err := os.Create("./.sunenv.yaml")       
 
-	if err != nil {
+switch arg {
 
-        fmt.Println(err)
+case "--author":
+
+			WriteYaml("author")
+
+case "--language":
+
+			WriteYaml("language")
+
+default:
+
+	WriteYaml("name")
 
 	}
 
-	defer f.Close()
-
-	var file_content string = fmt.Sprintf("name: %s", arg)
-
-	io.WriteString(f, file_content) 
 }
