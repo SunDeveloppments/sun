f01c4880b04cb6823b8e16afb63ea653a38d4e69
Jellyfish <oscar_b@disroot.org>
2025-06-14 10:16:33 +0200

    🚧 Worlink ...

--- src/init.go
+++ src/init.go
@@ -1,51 +1,83 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
-func WriteYaml(key string, value string) {
+func exists(path string) bool {
 
-	f, err := os.OpenFile("./.sunenv.yaml", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer f.Close()
+	_, err := os.Stat(path)
 
-	var file_content string = fmt.Sprintf("%s : %s\n", key, value)
+	return !errors.Is(err, os.ErrNotExist)
 
-	io.WriteString(f, file_content)
 }
 
-func Init() {
+func CreateFile() {
 
-	f_r := os.Remove("./.sunenv.yaml")
+	fc, e := os.Create("./.sunenv.yaml")
 
-	if f_r != nil {
-		fmt.Println("")
+	if e != nil {
+
+		fmt.Println(e)
+
 	}
 
-	if len(os.Args) >= 1 {
+	fc.Close()
 
-		name := flag.String("name", "Sun", "The name of your package")
-		language := flag.String("language", "Go", "The langauge in which is written your software")
-		author := flag.String("author", "Jellyfish", "You ( here me )")
+}
 
-		flag.Parse()
+func WriteYaml(key string, value string) {
 
-		WriteYaml("name", *name)
+	path := "./.sunenv.yaml"
 
-		WriteYaml("language", *language)
+	fe := exists(path)
 
-		WriteYaml("author", *author)
+	if fe == true {
+
+		CreateFile()
+
+		return
 
 	} else {
-		GreetInit()
 
+		f, err := os.OpenFile("./.sunenv.yaml", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+
+		if err != nil {
+
+			fmt.Println(err)
+
+		}
+
+		defer f.Close()
+
+		var file_content string = fmt.Sprintf("%s : %s\n", key, value)
+
+		io.WriteString(f, file_content)
 	}
+
+}
+func Init() {
+
+	// f_r := os.Remove("./.sunenv.yaml")
+
+	// if f_r != nil {
+	// fmt.Println("Reinit sun.")
+	// }
+
+	name := flag.String("name", "Sun", "The name of your package")
+	language := flag.String("language", "Go", "The langauge in which is written your software")
+	author := flag.String("author", "Jellyfish", "You ( here me )")
+
+	flag.Parse()
+
+	WriteYaml("name", *name)
+
+	WriteYaml("language", *language)
+
+	WriteYaml("author", *author)
 
 }
